c1f92841b341f40f2e5a9e4e343778a83c278e22
cliveportman <clive@cliveportman.co.uk>
2023-01-08 16:59:50 +0000

    restyling homepage

--- src/routes/Index.vue
+++ src/routes/Index.vue
@@ -1,13 +1,34 @@
 <script lang="ts" setup>
 import { routes } from '@/router/index'
+
+routes.shift() // Remove the link to the homepage
+
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center">
+  <div class="w-full ">
     <router-link
       v-for="(link, index) in routes"
       :to="link.path"
-      class="block text-3xl p-2 font-medium text-yellow-200 hover:opacity-50"
+      class="w-full block mb-0.5 p-4 text-center bg-yellow-100 bg-opacity-20 text-3xl font-medium text-yellow-200 hover:opacity-50"
+      >{{ link.name }}</router-link
+    >
+    <router-link
+      v-for="(link, index) in routes"
+      :to="link.path"
+      class="w-full block mb-0.5 p-4 text-center bg-yellow-100 bg-opacity-20 text-3xl font-medium text-yellow-200 hover:opacity-50"
+      >{{ link.name }}</router-link
+    >
+    <router-link
+      v-for="(link, index) in routes"
+      :to="link.path"
+      class="w-full block mb-0.5 p-4 text-center bg-yellow-100 bg-opacity-20 text-3xl font-medium text-yellow-200 hover:opacity-50"
+      >{{ link.name }}</router-link
+    >
+    <router-link
+      v-for="(link, index) in routes"
+      :to="link.path"
+      class="w-full block mb-0.5 p-4 text-center bg-yellow-100 bg-opacity-20 text-3xl font-medium text-yellow-200 hover:opacity-50"
       >{{ link.name }}</router-link
     >
   </div>
